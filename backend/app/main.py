55a50cdc16582b6ad99c5a6ae611925c793dff5a
GyurcsikMate <ovivankenobi3@gmail.com>
2024-07-02 18:13:13 +0200

    fixed backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,15 +10,36 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 """
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+"""
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, world!',
+    })
+"""
+
+"""
+beautifulsoup4
+requests
+psycopg2-binary
+fastapi
+uvicorn[standard]
+sqlalchemy
+"""
