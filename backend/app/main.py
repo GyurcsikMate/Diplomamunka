3ecbd2f7cf06a2e426f4148af9f698b74f1235b4
GyurcsikMate <ovivankenobi3@gmail.com>
2024-08-08 16:51:13 +0200

    get all works

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,26 +1,50 @@
-from fastapi import FastAPI, HTTPException, Query, Depends
-from sqlalchemy.orm import Session
+#import  models, schemas
+from fastapi import FastAPI
+from pymongo import MongoClient
+import os
 
-import crud, models, schemas
-from database import SessionLocal, engine
+app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+MONGO_DETAILS = os.getenv("MONGO_DETAILS", "mongodb://mongodb:27017")
+client = MongoClient(MONGO_DETAILS)
+database = client['test_database']
+collection = database['articles']
 
-app = FastAPI(
-    title="Articles",
-    description="Start using FastAPI in development",
-    version="0.1"
-)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close() 
+@app.get("/articles/{article_id}")
+def read_article(article_id: int):
+    article = collection.find_one({"article_id": article_id})
+    if article:
+        return {"article_id": article_id, "value": article["value"]}
+    return {"error": "Item not found"}
+
+@app.get("/articles")
+def read_articles():
+    articles = collection.find()
+    if articles:
+        return articles
+    return {"error": "Item not found"}
+
+@app.get("/articles/{article_id}")
+def insert_user(user: User):
+    result = await app.mongodb["users"].insert_one(user.dict())
+    inserted_user = await app.mongodb["users"].find_one({"_id": result.inserted_id})
+    return inserted_user
+def read_article(article_id: int):
+    article = collection.find_one({"article_id": article_id})
+    if article:
+        return {"article_id": article_id, "value": article["value"]}
+    return {"error": "Item not found"}
 
 
+
+
+
+
+"""
 @app.get('/articles/', response_model=schemas.ArticleBase, status_code=200)
 def get_article(symbol: str, db: Session = Depends(get_db)) -> models.Article:
     db_stock = crud.get_stock(db, symbol=symbol)
@@ -30,3 +54,4 @@ def get_article(symbol: str, db: Session = Depends(get_db)) -> models.Article:
         )
 
     return db_stock
+"""
